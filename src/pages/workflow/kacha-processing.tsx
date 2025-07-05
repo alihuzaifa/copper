fd6e75a04f015e8882a3e734d18cbf1631384f4b
alihuzaifa <ha4587323@gmail.com>
2025-07-05 19:14:20 +0500

    sell kacha also complete

--- src/pages/workflow/kacha-processing.tsx
+++ src/pages/workflow/kacha-processing.tsx
@@ -1,12 +1,14 @@
 import DashboardLayout from "@/components/layout/dashboard-layout";
 import AddKachaProcessing from "@/components/workflow/AddKachaProcessing";
 import KachaReturned from "@/components/workflow/KachaReturned";
+import SellKacha from "@/components/workflow/SellKacha";
 
 const KachaProcessingPage = () => {
   return (
     <DashboardLayout>
       <AddKachaProcessing />
       <KachaReturned />
+      <SellKacha />
     </DashboardLayout>
   );
 };
